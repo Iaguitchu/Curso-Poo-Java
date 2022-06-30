e8b516f59f86b86644198baa78b37f3117509366
Iaguitchu <iago2005andrade@gmail.com>
2022-06-30 00:31:28 -0300

    Ultimo commit

---
+++ Aula13/main.java
@@ -0,0 +1,9 @@
+package Aula13;
+
+public class main {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        c.reagir(11,45);
+        c.reagir(19,00);
+    }
+}
---
+++ Aula14/ProjetoYoutube.java
@@ -0,0 +1,11 @@
+package Aula14;
+
+public class ProjetoYoutube {
+    public static void main(String[] args) {
+        Gafanhoto g[] = new Gafanhoto[2];
+        g[0] = new Gafanhoto("Jubileu",22,"M","juba");
+        g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
+
+        System.out.println(g[0].toString());
+    }
+}
